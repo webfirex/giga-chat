8a417cd4d2a418dd989005111b6b42866d486903
Ashmit-Ag <ashmitamrit@gmail.com>
2026-01-14 18:24:26 +0530

    fixed error

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,11 +1,22 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
+  interface User extends DefaultUser {
+    id: string;
+    role: "ADMIN" | "MOD" | "USER";
+  }
+
   interface Session {
     user: {
       id: string;
-      email: string;
-      name?: string | null;
-    };
+      role: "ADMIN" | "MOD" | "USER";
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    role: "ADMIN" | "MOD" | "USER";
   }
 }
